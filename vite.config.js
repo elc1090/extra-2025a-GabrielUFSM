6e30d3899842c6e3724dd3e98642e8b5e7477c9c
GabrielUFSM <gbmoro@inf.ufsm.br>
2025-06-29 12:31:14 -0300

    fix: git pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/extra-2025a-GabrielUFSM/',
   plugins: [react()],
   publicDir: 'frontend/public',
 })
